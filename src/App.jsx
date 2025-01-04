16725986bca58ae55a5ff99d503128ea3cada072
Subhayan009kapas <subhayankapas47@gmail.com>
2025-01-04 11:38:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Footer_01 from './components/footer_comp/Footer_01';
+function App() {
+  return (
+    <>
+    <Footer_01/>
+ 
+    </>
+   
+  );
+}
+
+export default App;
